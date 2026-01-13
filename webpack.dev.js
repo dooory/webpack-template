56e54fe5aadd8744893be146a5dac5b2f3e89eed
dooory <78720364+dooory@users.noreply.github.com>
2026-01-13 14:40:21 +1300

    add template files

---
+++ webpack.dev.js
@@ -0,0 +1,10 @@
+const { default: merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+        static: "./dist",
+    },
+});
